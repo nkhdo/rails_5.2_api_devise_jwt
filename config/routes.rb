a81e36e8ea1fd586cb89f94a05c2848e27eb929b
Hoang Do <hoang.do@holistics.io>
2018-11-04 00:05:22 +0700

    Add devise-jwt auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api, defaults: {format: :json} do
+    devise_for :users, controllers: {sessions: 'sessions'}
+    devise_scope :user do
+      get 'users/current', to: 'sessions#show'
+    end
+  end
 end
